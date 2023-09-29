daefcfb8fecb4bf350c21948e36436b4c675de5f
Shapoval Denis <speleo04@yandex.ru>
2023-09-29 22:16:08 +0300

    Commit dz

--- src/java_course/AbstractHouse.java
+++ src/java_course/AbstractHouse.java
@@ -1,2 +1,27 @@
-package java_course;public class AbstractHouse {
+package java_course;
+
+public class AbstractHouse {
+
+        public String city;
+        String street;
+        protected int number;
+        private int apartment;
+
+        public AbstractHouse() {
+        }
+
+        public AbstractHouse(String city, String street, int number, int apartment) {
+            this.city = city;
+            this.street = street;
+            this.number = number;
+            this.apartment = apartment;
+        }
+
+        public int getApartment() {
+            return apartment;
+        }
+
+        void goToHouse() {
+        }
+
 }
--- src/java_course/NewHouse.java
+++ src/java_course/NewHouse.java
@@ -1,2 +1,17 @@
-package java_course;public class NewHouse {
+package java_course;
+
+public class NewHouse extends AbstractHouse {
+    public NewHouse() {
+
+    }
+
+    public NewHouse(String city, String street, int number, int apartment) {
+        super(city, street, number, apartment);
+
+    }
+
+    @Override
+    void goToHouse() {
+        super.goToHouse();
+    }
 }
